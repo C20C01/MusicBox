2be6b586f7ad98d4959d00d42c7120cfc3d5fdef
CC <cc_20_01@163.com>
2024-08-09 21:28:44 +0800

    NeoForge-1.21 完善README 播放器中添加了对页数的数值检查，防止纸带数据损坏（跨版本等原因）导致数组越界。

--- src/main/java/io/github/c20c01/cc_mb/util/player/MusicBoxPlayer.java
+++ src/main/java/io/github/c20c01/cc_mb/util/player/MusicBoxPlayer.java
@@ -1,5 +1,6 @@
 package io.github.c20c01.cc_mb.util.player;
 
+import com.mojang.logging.LogUtils;
 import io.github.c20c01.cc_mb.block.MusicBoxBlock;
 import io.github.c20c01.cc_mb.block.entity.SoundBoxBlockEntity;
 import io.github.c20c01.cc_mb.data.Beat;
@@ -13,7 +14,6 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.util.Mth;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
@@ -68,7 +68,7 @@ public class MusicBoxPlayer extends AbstractNoteGridPlayer {
         setTickPerBeat(tag.getByte("tick_per_beat"));
         tickSinceLastBeat = tag.getByte("interval");
         beatNumber = tag.getByte("beat");
-        pageNumber = tag.getByte("page");
+        setPageNumber(tag.getByte("page"));
     }
 
     public void saveAdditional(CompoundTag tag) {
@@ -83,7 +83,7 @@ public class MusicBoxPlayer extends AbstractNoteGridPlayer {
         setTickPerBeat(data[0]);
         tickSinceLastBeat = data[1];
         beatNumber = data[2];
-        pageNumber = data[3];
+        setPageNumber(data[3]);
     }
 
     public void saveUpdateTag(CompoundTag tag) {
@@ -102,7 +102,16 @@ public class MusicBoxPlayer extends AbstractNoteGridPlayer {
 
     public void setData(@Nullable NoteGridData data) {
         this.data = data;
-        this.pageNumber = (byte) Mth.clamp(this.pageNumber, 0, dataSize() - 1);
+        setPageNumber(pageNumber);
+    }
+
+    private void setPageNumber(byte pageNumber) {
+        if (pageNumber >= dataSize()) {
+            this.pageNumber = 0;
+            LogUtils.getLogger().warn("Page number is out of range. Is the data changed?");
+        } else {
+            this.pageNumber = pageNumber;
+        }
     }
 
     /**
