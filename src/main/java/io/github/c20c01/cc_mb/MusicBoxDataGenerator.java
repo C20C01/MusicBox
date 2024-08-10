dd5c701566519c938eb9663a77d3f306cc1e943b
CC <cc_20_01@163.com>
2024-08-10 16:59:59 +0800

    Fabric-1.20.1 初始提交，代码还没动。

---
+++ src/main/java/io/github/c20c01/cc_mb/MusicBoxDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.c20c01.cc_mb;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MusicBoxDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
