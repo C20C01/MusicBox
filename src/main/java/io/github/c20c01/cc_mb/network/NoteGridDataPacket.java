795362e70f03042f8c92faebba35dc5120e13504
CC <cc_20_01@163.com>
2024-12-03 11:39:07 +0800

    改进代码？

--- src/main/java/io/github/c20c01/cc_mb/network/NoteGridDataPacket.java
+++ src/main/java/io/github/c20c01/cc_mb/network/NoteGridDataPacket.java
@@ -7,14 +7,30 @@ import io.github.c20c01.cc_mb.client.NoteGridDataManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.network.NetworkEvent;
 
+import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
 public class NoteGridDataPacket {
     public record ToServer(int hash, BlockPos blockPos) {
         public static NoteGridDataPacket.ToServer decode(FriendlyByteBuf friendlyByteBuf) {
             return new NoteGridDataPacket.ToServer(friendlyByteBuf.readInt(), friendlyByteBuf.readBlockPos());
+        }
+
+        private static boolean isValid(BlockPos targetPos, @Nullable Player player) {
+            if (player == null) {
+                return false;
+            }
+            BlockPos playerPos = player.blockPosition();
+            double disSqr = playerPos.distSqr(targetPos);
+            if (disSqr >= 4096) {
+                LogUtils.getLogger().warn("{} at {} requested data from {} which is too far away ({}).",
+                        player.getDisplayName(), playerPos, targetPos, Math.sqrt(disSqr));
+                return false;
+            }
+            return true;
         }
 
         private static void tryToReply(NetworkEvent.Context context, int hash, BlockPos blockPos) {
@@ -25,21 +41,6 @@ public class NoteGridDataPacket {
                     .ifPresent(noteGridData -> CCNetwork.CHANNEL.reply(new ToClient(hash, noteGridData.toBytes()), context));
         }
 
-        private boolean isValid(NetworkEvent.Context context) {
-            ServerPlayer player = context.getSender();
-            if (player == null) {
-                return false;
-            }
-            BlockPos playerPos = player.blockPosition();
-            double disSqr = playerPos.distSqr(blockPos);
-            if (disSqr >= 4096) {
-                LogUtils.getLogger().warn("{} at {} requested data from {} which is too far away ({}).",
-                        player.getDisplayName(), playerPos, blockPos, Math.sqrt(disSqr));
-                return false;
-            }
-            return true;
-        }
-
         public void encode(FriendlyByteBuf friendlyByteBuf) {
             friendlyByteBuf.writeInt(hash);
             friendlyByteBuf.writeBlockPos(blockPos);
@@ -48,7 +49,7 @@ public class NoteGridDataPacket {
         public void handleOnServer(Supplier<NetworkEvent.Context> contextSupplier) {
             NetworkEvent.Context context = contextSupplier.get();
             context.enqueueWork(() -> {
-                if (isValid(context)) {
+                if (isValid(blockPos, context.getSender())) {
                     tryToReply(context, hash, blockPos);
                 }
             });
