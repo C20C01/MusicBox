6bbf6c44d9905f75a1d6ef108adc6a7e31a700ed
CC <cc_20_01@163.com>
2025-05-11 13:43:58 +0800

    mixin的准备工作

---
+++ src/main/java/io/github/c20c01/cc_mb/mixin/MixinSoundEngine.java
@@ -0,0 +1,9 @@
+package io.github.c20c01.cc_mb.mixin;
+
+import net.minecraft.client.sounds.SoundEngine;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SoundEngine.class)
+public class MixinSoundEngine {
+
+}
