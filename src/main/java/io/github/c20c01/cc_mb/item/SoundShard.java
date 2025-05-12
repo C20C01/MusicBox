8a57515f0ccad7542a91d9a838b452a14335d1aa
CC <cc_20_01@163.com>
2025-05-12 22:00:41 +0800

    修复“声响碎片发声时声源不会随玩家的移动而移动”的问题

--- src/main/java/io/github/c20c01/cc_mb/item/SoundShard.java
+++ src/main/java/io/github/c20c01/cc_mb/item/SoundShard.java
@@ -115,7 +115,7 @@ public class SoundShard extends Item {
                 // creative mode only: shift to change the sound seed.
                 Long newSeed = tryToChangeSoundSeed(soundShard, level.random);
                 if (newSeed != null) {
-                    level.playSeededSound(null, player.getX(), player.getY(), player.getZ(), info.sound(), player.getSoundSource(), 1.0F, 1.0F, newSeed);
+                    level.playSeededSound(player, player, info.sound(), player.getSoundSource(), 1.0F, 1.0F, newSeed);
                 }
                 return InteractionResultHolder.sidedSuccess(soundShard, level.isClientSide);
             }
@@ -130,7 +130,7 @@ public class SoundShard extends Item {
             }
         }
         // play the sound event that saved in the sound shard
-        level.playSeededSound(null, player.getX(), player.getY(), player.getZ(), info.sound(), player.getSoundSource(), 1.0F, 1.0F, info.seed() == null ? level.random.nextLong() : info.seed());
+        level.playSeededSound(player, player, info.sound(), player.getSoundSource(), 1.0F, 1.0F, info.seed() == null ? level.random.nextLong() : info.seed());
         level.gameEvent(player, GameEvent.INSTRUMENT_PLAY, player.position());
         return InteractionResultHolder.sidedSuccess(soundShard, level.isClientSide);
     }
